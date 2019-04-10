1f25bd036e104a9ba22ae4eb4bc351a8a18411c1
Natalia PN <npelegero@gmail.com>
2019-04-10 22:01:41 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import handTap from './images/gesture-tap-hold.svg';
 import magnify from './images/magnify.svg';
 import heart from './images/heart.svg';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
@@ -38,16 +39,11 @@ class App extends Component {
     
     return (
       <div className="App">
-        <header className="App-header">
-          <img className="Hand-tap" src={handTap} alt="hand tapping icon"></img>
-          
-          <label htmlFor="search-field" className="Search-field__label"></label>
-          <input type="search" id="search-field" className="Search-field__input" placeholder="Search" onKeyUp={getSearchName}/>
-          <img className="Magnify" src={magnify} alt="magnifying glass icon"></img>
-
-          <img className="Heart" src={heart} alt="heart icon"></img>
-          <span className="Counter">1</span>
-        </header>
+        <Header  
+          handTap={handTap}
+          getSearchName={getSearchName} 
+          magnify={magnify}
+          heart={heart} />
 
         <main className="Main-section">
           <ul className="Songs__list">
