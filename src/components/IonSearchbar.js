dfb60f71da22ccd0d47b67b99a0e93f5151c748a
Natalia PN <npelegero@gmail.com>
2019-04-11 18:44:05 +0200

    Add styles to header

--- src/components/IonSearchbar.js
+++ src/components/IonSearchbar.js
@@ -3,15 +3,14 @@ import PropTypes from "prop-types";
 
 class IonSearchbar extends Component {
     render() {
-        const { getSearchName, magnify } = this.props;
+        const { getSearchName } = this.props;
 
         return(
             <div className="Search-bar">
                 <label htmlFor="search-field" className="Search-field__label"></label>
-                <input type="search" id="search-field" className="Search-field__input" placeholder="Search" onChange={getSearchName}/>   
+                <input type="text" id="search-field" className="Search-field__input" placeholder="Search" onChange={getSearchName}/>   
 
-                {/* <img className="Magnify" src={magnify} alt="magnifying glass icon"></img> */}
-                <i class="fas fa-search"></i>
+                <i class="material-icons Magnify">search</i>
             </div>
         );
     }
@@ -19,7 +18,6 @@ class IonSearchbar extends Component {
 
 IonSearchbar.propTypes = {
     getSearchName: PropTypes.func,
-    magnify:PropTypes.string,
 }
 
 export default IonSearchbar;
